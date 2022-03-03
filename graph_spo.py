467c82db9514ffaf2b23dcb71d32347f63722a79
kbr1009 <reo109r@icloud.com>
2022-03-03 18:31:30 +0900

    誤字を修正

--- graph_spo.py
+++ graph_spo.py
@@ -6,18 +6,15 @@ import requests, json, re
 
 
 class SpoAPI:
-    """
-    認証情報等
-    """
+
+    #認証情報等
     def __init__(self):
         self.__tenant_id = "XXXX"
         self.__app_id = "XXXX"
         self.__secret = "XXXX"
         self.__site_id = "XXXXX"
 
-    """
-    apiのアクセストークンを取得する間数(private)
-    """
+    #apiのアクセストークンを取得する関数(private)
     def __get_token(self):
         auth_url = "https://login.microsoftonline.com/"+ self.__tenant_id +"/oauth2/v2.0/token"
         headers = {
@@ -33,9 +30,7 @@ class SpoAPI:
         res = requests.post(auth_url, headers=headers, data=payload)
         return res.json()['access_token']
 
-    """
-    ファイルを取得する間数
-    """
+    #ファイルを取得する関数
     def access_graph(self):
         url = "https://graph.microsoft.com/v1.0/sites/"+ self.__site_id +"/drive/items/root/children"
         token = self.__get_token()
@@ -45,8 +40,13 @@ class SpoAPI:
         res = requests.get(url, headers=headers)
         payload = []
 
+        #graphに問い合わせて返却された情報をhtmlに渡すための配列を作成
         for i in res.json()['value']:
+
+            # 共有idを抽出
             item_id = "{" + re.findall("(?<=\=).+?(?=\&)", i['@microsoft.graph.downloadUrl'])[0] + "}"
+
+            # 共有id以外のurlの要素を直打ちしているが、apiを叩いて代入することも可能
             payload.append(
                     {
                         'name': i['name'],
@@ -57,7 +57,7 @@ class SpoAPI:
         return payload
 
 
-
+#このスクリプトが実行された時に行われる処理
 if __name__=='__main__':
     spo_file = SpoAPI().access_graph()
     context = {'spo_file': spo_file}
